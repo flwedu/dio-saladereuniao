75ed9c8f24288dae9c4c7a4783bfb93d54517afd
flwedu <edutraquino@gmail.com>
2021-08-13 20:16:41 -0300

    add CRUD methods

--- src/main/java/com/digital/one/saladereuniao/service/RoomService.java
+++ src/main/java/com/digital/one/saladereuniao/service/RoomService.java
@@ -1,12 +1,32 @@
 package com.digital.one.saladereuniao.service;
 
+import com.digital.one.saladereuniao.model.Room;
 import com.digital.one.saladereuniao.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomService {
 
     @Autowired
     private RoomRepository roomRepository;
+
+    public List<Room> findAll() {
+        return roomRepository.findAll();
+    }
+
+    public Optional<Room> findById(Long id) {
+        return roomRepository.findById(id);
+    }
+
+    public Room save(Room room) {
+        return roomRepository.save(room);
+    }
+
+    public void deleteById(Long id){
+        roomRepository.deleteById(id);
+    }
 }
