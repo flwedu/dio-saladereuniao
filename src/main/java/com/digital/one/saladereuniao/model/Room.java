0c28e7f6ffe5508450599a3b383c0857a011757e
flwedu <edutraquino@gmail.com>
2021-08-15 19:39:54 -0300

    Add validations

--- src/main/java/com/digital/one/saladereuniao/model/Room.java
+++ src/main/java/com/digital/one/saladereuniao/model/Room.java
@@ -1,12 +1,18 @@
 package com.digital.one.saladereuniao.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "meetingrooms")
@@ -20,21 +26,22 @@ public class Room {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotNull
+    @Length(min = 2, max = 25)
     private String name;
 
     @NotNull
     private String date;
+
+    @NotNull
     private String startHour;
+
+    @NotNull
     private String endHour;
 
     @Override
     public String toString() {
-        return "Room{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", date='" + date + '\'' +
-                ", startHour='" + startHour + '\'' +
-                ", endHour='" + endHour + '\'' +
-                '}';
+        return "Room{" + "id=" + id + ", name='" + name + '\'' + ", date='" + date + '\'' + ", startHour='" + startHour
+                + '\'' + ", endHour='" + endHour + '\'' + '}';
     }
 }
