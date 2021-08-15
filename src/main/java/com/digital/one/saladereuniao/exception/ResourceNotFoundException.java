f261bedcf2472844cf5654d8b8b1d56dcffaeffe
flwedu <edutraquino@gmail.com>
2021-08-15 19:43:08 -0300

    removed unused atribute (SerialVersionUID)

--- src/main/java/com/digital/one/saladereuniao/exception/ResourceNotFoundException.java
+++ src/main/java/com/digital/one/saladereuniao/exception/ResourceNotFoundException.java
@@ -1,15 +1,12 @@
 package com.digital.one.saladereuniao.exception;
 
-import javassist.SerialVersionUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends Exception{
+public class ResourceNotFoundException extends Exception {
 
-    private static final long SerialVersionUID = 1L;
-
-    public ResourceNotFoundException(String message){
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 }
