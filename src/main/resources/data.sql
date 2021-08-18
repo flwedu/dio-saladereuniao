88265f8e72620f8a38fcf79de8ce39be059f887b
flwedu <edutraquino@gmail.com>
2021-08-17 21:08:35 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,10 @@
-DROP TABLE IF EXISTS meetingrooms;
-
 CREATE TABLE meetingrooms (
-    id AUTOINCREMENT PRIMARY KEY NOT NULL,
+    id INTEGER(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(25) NOT NULL,
     date VARCHAR(15) NOT NULL,
     startHour VARCHAR(6) NOT NULL,
     endHour VARCHAR(6) NOT NULL
-)
+);
 
 INSERT INTO meetingrooms (name, date, startHour, endHour) VALUES
 ('Sala Principal', '15/08/2021', '09:00', '12:00'),
