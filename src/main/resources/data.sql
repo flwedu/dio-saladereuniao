e9e7ec8dde07214a07157dcf2c964d4724141c4c
flwedu <edutraquino@gmail.com>
2021-09-01 22:03:31 -0300

    initial data to h2-database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,18 @@
-INSERT INTO meetingrooms (name, date, startHour, endHour) VALUES
+drop table if exists meetingrooms CASCADE ;
+
+drop sequence if exists hibernate_sequence;
+
+create sequence hibernate_sequence start with 1 increment by 1;
+
+create table meetingrooms (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        date varchar(255) not null,
+        end_hour varchar(255) not null,
+        name varchar(25) not null,
+        start_hour varchar(255) not null
+);
+
+INSERT INTO meetingrooms (name, date, start_hour, end_hour) VALUES
 ('Sala Principal', '15/08/2021', '09:00', '12:00'),
 ('Sala 2', '15/08/2021', '09:00', '11:30'),
 ('Sala 3', '15/08/2021', '10:00', '14:00');
--- src/main/resources/schema-h2.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE meetingrooms (
-    id INTEGER(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(25) NOT NULL,
-    date VARCHAR(15) NOT NULL,
-    startHour VARCHAR(6) NOT NULL,
-    endHour VARCHAR(6) NOT NULL
-);
