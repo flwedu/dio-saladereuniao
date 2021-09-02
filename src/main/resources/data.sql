46ac89c39955dcaee9732daed89d9dc1c198ab0f
flwedu <edutraquino@gmail.com>
2021-09-01 21:30:58 -0300

    populating h2 database with initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,3 @@
-CREATE TABLE meetingrooms (
-    id INTEGER(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(25) NOT NULL,
-    date VARCHAR(15) NOT NULL,
-    startHour VARCHAR(6) NOT NULL,
-    endHour VARCHAR(6) NOT NULL
-);
-
 INSERT INTO meetingrooms (name, date, startHour, endHour) VALUES
 ('Sala Principal', '15/08/2021', '09:00', '12:00'),
 ('Sala 2', '15/08/2021', '09:00', '11:30'),
