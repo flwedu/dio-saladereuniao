be74e9a2590b70ec081fb98fd973dcfc8a3ac7e9
Eduardo <edutraquino@gmail.com>
2021-10-09 19:04:42 -0300

    created test class for RoomService

---
+++ src/test/java/com/digital/one/saladereuniao/service/RoomServiceTest.java
@@ -0,0 +1,16 @@
+package com.digital.one.saladereuniao.service;
+
+import com.digital.one.saladereuniao.repository.RoomRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RoomServiceTest {
+
+    private RoomRepository repository;
+
+    @Test
+    @DisplayName("Should return a list with all rooms")
+    void shouldReturnAListWithAllRooms(){
+
+    }
+}
