477521cad71c10d9c59692da02a1f4f0bf46d6f5
Eduardo <edutraquino@gmail.com>
2021-12-07 22:13:48 -0300

    fix imports mokito

--- src/test/java/com/digital/one/saladereuniao/service/RoomServiceTest.java
+++ src/test/java/com/digital/one/saladereuniao/service/RoomServiceTest.java
@@ -1,5 +1,7 @@
 package com.digital.one.saladereuniao.service;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -20,17 +22,18 @@ public class RoomServiceTest {
     private static RoomRepository repository;
     private static RoomService service;
 
-    static List<Room> mockList = Arrays.asList(new Room(1L, "Room 1", "2021-10-09", "10:00", "12:00"),
-	    new Room(2L, "Room 2", "2021-10-09", "10:00", "12:00"),
-	    new Room(3L, "Room 3", "2021-10-09", "10:00", "12:00"));
+    static List<Room> mockList = Arrays.asList(
+            new Room(1L, "Room 1", LocalDate.now().plusDays(2), LocalTime.of(10, 0), LocalTime.NOON),
+            new Room(2L, "Room 2", LocalDate.now().plusDays(1), LocalTime.of(10, 0), LocalTime.NOON),
+            new Room(3L, "Room 3", LocalDate.now(), LocalTime.of(10, 0), LocalTime.NOON));
 
     @BeforeAll
     public static void setup() {
 
-	repository = Mockito.mock(RoomRepository.class);
-	service = new RoomService(repository);
+        repository = Mockito.mock(RoomRepository.class);
+        service = new RoomService(repository);
 
-	Mockito.when(repository.findAll()).thenReturn(mockList);
+        Mockito.when(repository.findAll()).thenReturn(mockList);
 
     }
 
@@ -38,8 +41,8 @@ public class RoomServiceTest {
     @DisplayName("Should return a list with all rooms")
     void shouldReturnAListWithAllRooms() {
 
-	List<Room> returnedList = service.findAll();
-	Assertions.assertEquals(mockList, returnedList);
+        List<Room> returnedList = service.findAll();
+        Assertions.assertEquals(mockList, returnedList);
     }
 
     @DisplayName("Should find a room")
@@ -47,13 +50,13 @@ public class RoomServiceTest {
     @ValueSource(longs = { 1L, 2L, 3L })
     void shouldFindAnElement(Long id) {
 
-	Optional<Room> findedInMock = mockList.stream().filter(room -> room.getId().equals(id)).findFirst();
-	Mockito.when(repository.findById(id)).thenReturn(findedInMock);
+        Optional<Room> findedInMock = mockList.stream().filter(room -> room.getId().equals(id)).findFirst();
+        Mockito.when(repository.findById(id)).thenReturn(findedInMock);
 
-	Optional<Room> findedInService = service.findById(id);
+        Optional<Room> findedInService = service.findById(id);
 
-	Assertions.assertTrue(findedInService.isPresent());
-	Assertions.assertEquals(findedInService, findedInMock);
+        Assertions.assertTrue(findedInService.isPresent());
+        Assertions.assertEquals(findedInService, findedInMock);
 
     }
 
@@ -62,12 +65,12 @@ public class RoomServiceTest {
     @ValueSource(longs = { 4L, 5L, 6L })
     void shouldNotFindAnElement(Long id) {
 
-	Optional<Room> findedInMock = mockList.stream().filter(room -> room.getId().equals(id)).findFirst();
-	Mockito.when(repository.findById(id)).thenReturn(findedInMock);
+        Optional<Room> findedInMock = mockList.stream().filter(room -> room.getId().equals(id)).findFirst();
+        Mockito.when(repository.findById(id)).thenReturn(findedInMock);
 
-	Optional<Room> findedInService = service.findById(id);
+        Optional<Room> findedInService = service.findById(id);
 
-	Assertions.assertTrue(findedInService.isEmpty());
+        Assertions.assertTrue(findedInService.isEmpty());
 
     }
 }
