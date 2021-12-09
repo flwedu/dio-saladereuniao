20e3ba1f8c9958c6b0f2347a492f019f94f07806
Eduardo <edutraquino@gmail.com>
2021-12-09 18:10:40 -0300

    add tests: List mapping from Entity to DTO

--- src/test/java/com/digital/one/saladereuniao/DTO/RoomMapperTest.java
+++ src/test/java/com/digital/one/saladereuniao/DTO/RoomMapperTest.java
@@ -1,9 +1,12 @@
 package com.digital.one.saladereuniao.DTO;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.digital.one.saladereuniao.model.Room;
 
@@ -12,10 +15,12 @@ import org.junit.jupiter.api.Test;
 
 class RoomMapperTest {
 
-    private static Room expectedRoom = new Room(1L, "Test", LocalDate.of(2021, 12, 01), LocalTime.of(11, 00, 00),
+    private static Room expectedRoom = new Room(
+            1L, "Test", LocalDate.of(2021, 12, 01), LocalTime.of(11, 00, 00),
             LocalTime.of(10, 00, 00));
 
-    private static RoomDTO expectedRoomDto = new RoomDTO(1L, "Test", LocalDate.of(2021, 12, 01),
+    private static RoomDTO expectedRoomDto = new RoomDTO(
+            1L, "Test", LocalDate.of(2021, 12, 01),
             LocalTime.of(11, 00, 00),
             LocalTime.of(10, 00, 00));
 
@@ -33,6 +38,31 @@ class RoomMapperTest {
 
         Room testRoom = RoomMapper.toEntity(expectedRoomDto);
         assertEquals(testRoom, expectedRoom);
+    }
+
+    @Test
+    @DisplayName("Should convert a List of Entities to a List of DTO's")
+    void shouldConvertAnListOfEntitiesToDto() {
+
+        List<Room> roomsList = List.of(
+                new Room(1L, "Test", LocalDate.of(2021, 12, 01), LocalTime.of(11, 00, 00),
+                        LocalTime.of(10, 00, 00)),
+                new Room(2L, "Test", LocalDate.of(2021, 12, 01), LocalTime.of(11, 00, 00),
+                        LocalTime.of(10, 00, 00)),
+                new Room(3L, "Test", LocalDate.of(2021, 12, 01), LocalTime.of(11, 00, 00),
+                        LocalTime.of(10, 00, 00)));
+
+        List<RoomDTO> expectedRoomDtoList = List.of(
+                new RoomDTO(1L, "Test", LocalDate.of(2021, 12, 01), LocalTime.of(11, 00, 00),
+                        LocalTime.of(10, 00, 00)),
+                new RoomDTO(2L, "Test", LocalDate.of(2021, 12, 01), LocalTime.of(11, 00, 00),
+                        LocalTime.of(10, 00, 00)),
+                new RoomDTO(3L, "Test", LocalDate.of(2021, 12, 01), LocalTime.of(11, 00, 00),
+                        LocalTime.of(10, 00, 00)));
+
+        List<RoomDTO> convertedRoomDto = roomsList.stream().map(RoomMapper::toDTO).collect(Collectors.toList());
+
+        assertIterableEquals(expectedRoomDtoList, convertedRoomDto);
     }
 
 }
