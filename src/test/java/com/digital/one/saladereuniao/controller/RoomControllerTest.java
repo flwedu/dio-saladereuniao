af3035d824523fb3e0c125122086d2824ca12466
Eduardo <edutraquino@gmail.com>
2021-12-20 17:53:31 -0300

    add params to new test room

--- src/test/java/com/digital/one/saladereuniao/controller/RoomControllerTest.java
+++ src/test/java/com/digital/one/saladereuniao/controller/RoomControllerTest.java
@@ -1,5 +1,7 @@
 package com.digital.one.saladereuniao.controller;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,19 +32,20 @@ public class RoomControllerTest {
 
     @BeforeEach
     public void setup() {
-	RestAssuredMockMvc.standaloneSetup(this.roomController);
+        RestAssuredMockMvc.standaloneSetup(this.roomController);
     }
 
     @Test
     @DisplayName("Should return Sucess (200) HTTP status code when searched for one existing room")
     public void shouldReturnSucess_WhenSearchedForRoom() {
 
-	Optional<Room> room = Optional.of(new Room());
+        Optional<Room> room = Optional.of(new Room(1L, "Room 1", LocalDate.now().plusDays(2), LocalTime.of(10, 0),
+                LocalTime.NOON));
 
-	Mockito.when(roomService.findById(1L)).thenReturn(room);
+        Mockito.when(roomService.findById(1L)).thenReturn(room);
 
-	RestAssuredMockMvc.given().accept(ContentType.JSON).when().get("/api/v1/rooms/{id}", 1L).then()
-		.statusCode(HttpStatus.OK.value());
+        RestAssuredMockMvc.given().accept(ContentType.JSON).when().get("/api/v1/rooms/{id}", 1L).then()
+                .statusCode(HttpStatus.OK.value());
 
     }
 
@@ -50,12 +53,12 @@ public class RoomControllerTest {
     @DisplayName("Should return Sucess (200) HTTP status code when searched for all rooms list")
     public void shouldReturnSucess_WhenSearchedForAllRoomsList() {
 
-	List<Room> list = List.of(new Room());
+        List<Room> list = List.of(new Room());
 
-	Mockito.when(roomService.findAll()).thenReturn(list);
+        Mockito.when(roomService.findAll()).thenReturn(list);
 
-	RestAssuredMockMvc.given().accept(ContentType.JSON).when().get("/api/v1/rooms/").then()
-		.statusCode(HttpStatus.OK.value());
+        RestAssuredMockMvc.given().accept(ContentType.JSON).when().get("/api/v1/rooms/").then()
+                .statusCode(HttpStatus.OK.value());
 
     }
 
@@ -63,11 +66,11 @@ public class RoomControllerTest {
     @DisplayName("Should return Not Found (404) HTTP status code when don't found a room resource")
     public void shouldReturnNotFound_WhenSearchedForANonexistentRoom() {
 
-	Optional<Room> notARoom = Optional.empty();
-	Mockito.when(roomService.findById(1L)).thenReturn(notARoom);
+        Optional<Room> notARoom = Optional.empty();
+        Mockito.when(roomService.findById(1L)).thenReturn(notARoom);
 
-	RestAssuredMockMvc.given().accept(ContentType.JSON).when().get("/api/v1/rooms/{id}", 1L).then()
-		.statusCode(HttpStatus.NOT_FOUND.value());
+        RestAssuredMockMvc.given().accept(ContentType.JSON).when().get("/api/v1/rooms/{id}", 1L).then()
+                .statusCode(HttpStatus.NOT_FOUND.value());
 
     }
 
