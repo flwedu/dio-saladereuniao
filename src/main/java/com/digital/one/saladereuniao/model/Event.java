465d63ffcf9537ce271ccf617d712b6da2c0eac7
Eduardo <edutraquino@gmail.com>
2021-12-21 20:22:34 -0300

    add relationship to room

--- src/main/java/com/digital/one/saladereuniao/model/Event.java
+++ src/main/java/com/digital/one/saladereuniao/model/Event.java
@@ -7,11 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name = "events")
 public class Event {
@@ -20,10 +23,12 @@ public class Event {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @ManyToOne
     Room room;
+
     @Column(nullable = false)
     private LocalDateTime startTime;
+
     @Column(nullable = false)
     private LocalDateTime endingTime;
 }
