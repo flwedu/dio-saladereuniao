f09c65c6342d6a5584eff2906a7555b27df28d5e
Eduardo <edutraquino@gmail.com>
2021-12-21 18:00:23 -0300

    put mappin returns 201 code

--- src/main/java/com/digital/one/saladereuniao/controler/RoomController.java
+++ src/main/java/com/digital/one/saladereuniao/controler/RoomController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -54,7 +55,7 @@ public class RoomController {
             throws ResourceNotFoundException {
         findRoom(id);
         Room updatedRoom = roomService.save(newRoomData.toEntity());
-        return ResponseEntity.ok(updatedRoom.toDTO());
+        return new ResponseEntity<RoomDTO>(updatedRoom.toDTO(), HttpStatus.ACCEPTED);
 
     }
 
