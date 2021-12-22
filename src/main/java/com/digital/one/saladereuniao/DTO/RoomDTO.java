4221d6c9ccde9bffa9cf884ac4e2c55388e4bb69
Eduardo <edutraquino@gmail.com>
2021-12-22 18:27:15 -0300

    organize imports

--- src/main/java/com/digital/one/saladereuniao/DTO/RoomDTO.java
+++ src/main/java/com/digital/one/saladereuniao/DTO/RoomDTO.java
@@ -1,14 +1,10 @@
 package com.digital.one.saladereuniao.DTO;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.Length;
-
 import com.digital.one.saladereuniao.model.Room;
-import com.fasterxml.jackson.annotation.JsonFormat;
+
+import org.hibernate.validator.constraints.Length;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
