e873ec8dbc074a476da038cea23f08588db412ee
Eduardo <edutraquino@gmail.com>
2021-12-22 19:12:53 -0300

    add findAllByRoomId

--- src/main/java/com/digital/one/saladereuniao/repository/EventRepository.java
+++ src/main/java/com/digital/one/saladereuniao/repository/EventRepository.java
@@ -1,8 +1,13 @@
 package com.digital.one.saladereuniao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 import com.digital.one.saladereuniao.model.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    List<Event> findAllByRoomId(Long roomId);
 
 }
