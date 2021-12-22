dd0d757364913e8c48510f56a816659746cd5894
Eduardo <edutraquino@gmail.com>
2021-12-22 19:22:25 -0300

    add @Repository

--- src/main/java/com/digital/one/saladereuniao/repository/EventRepository.java
+++ src/main/java/com/digital/one/saladereuniao/repository/EventRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.digital.one.saladereuniao.model.Event;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
     List<Event> findAllByRoomId(Long roomId);
