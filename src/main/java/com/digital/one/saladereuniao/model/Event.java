c86bb9772f96fe9c24b5a843eb36a1524264d870
Eduardo <edutraquino@gmail.com>
2021-12-22 20:33:02 -0300

    add allArgsConstructor

--- src/main/java/com/digital/one/saladereuniao/model/Event.java
+++ src/main/java/com/digital/one/saladereuniao/model/Event.java
@@ -10,10 +10,12 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "events")
