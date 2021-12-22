cf81212d6caa1106d5a492cfb232de01c3e2dd51
Eduardo <edutraquino@gmail.com>
2021-12-22 18:50:27 -0300

    creating faker class to generate objects to test

---
+++ src/test/java/com/digital/one/saladereuniao/utils/RoomFaker.java
@@ -0,0 +1,18 @@
+package com.digital.one.saladereuniao.utils;
+
+import java.util.List;
+
+import com.digital.one.saladereuniao.DTO.RoomDTO;
+import com.digital.one.saladereuniao.model.Room;
+
+public class RoomFaker {
+
+    public static Room createFakeRoom(Long id) {
+        return new Room(id, "Room 1", "An test room", List.of());
+    }
+
+    public static RoomDTO createFakeRoomDto(Long id) {
+        return new RoomDTO(id, "Room 1", "An test room");
+    }
+
+}
