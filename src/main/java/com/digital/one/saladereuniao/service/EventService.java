c3b14aec8794cd632706a1aa332441b59d163dfe
Eduardo <edutraquino@gmail.com>
2021-12-23 07:44:04 -0300

    rename repository and mv autowired annotation

--- src/main/java/com/digital/one/saladereuniao/service/EventService.java
+++ src/main/java/com/digital/one/saladereuniao/service/EventService.java
@@ -5,38 +5,39 @@ import java.util.Optional;
 import com.digital.one.saladereuniao.model.Event;
 import com.digital.one.saladereuniao.repository.EventRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventService {
 
-    private EventRepository repository;
+    private EventRepository eventRepository;
 
-    public EventService(@Autowired EventRepository repository) {
-        this.repository = repository;
+    @Autowired
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
     }
 
     public Page<Event> findAllByRoomId(Long roomId, Pageable pageable) {
-        return this.repository.findAllByRoomId(roomId, pageable);
+        return eventRepository.findAllByRoomId(roomId, pageable);
     }
 
     public Page<Event> findAll(Pageable pageable) {
-        return this.repository.findAll(pageable);
+        return eventRepository.findAll(pageable);
     }
 
     public Optional<Event> findById(Long id) {
-        return this.repository.findById(id);
+        return eventRepository.findById(id);
     }
 
     public Event save(Event event) {
-        return this.repository.save(event);
+        return eventRepository.save(event);
     }
 
     public void delete(Long id) {
-        this.repository.deleteById(id);
+        eventRepository.deleteById(id);
     }
 
 }
