32e5ee06c088781857738f5870aa4e9cee4b5203
Eduardo <edutraquino@gmail.com>
2021-12-25 16:37:32 -0300

    add save (POST) method

--- src/main/java/com/digital/one/saladereuniao/controler/RoomEventController.java
+++ src/main/java/com/digital/one/saladereuniao/controler/RoomEventController.java
@@ -1,5 +1,9 @@
 package com.digital.one.saladereuniao.controler;
 
+import javax.validation.Valid;
+
+import com.digital.one.saladereuniao.DTO.RoomEventDTO;
+import com.digital.one.saladereuniao.model.Room;
 import com.digital.one.saladereuniao.model.RoomEvent;
 import com.digital.one.saladereuniao.service.RoomEventService;
 import com.digital.one.saladereuniao.service.RoomService;
@@ -10,6 +14,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +48,16 @@ public class RoomEventController {
 
         return ResponseEntity.ok().body(eventService.findAllByRoomId(roomId, page));
 
+    }
+
+    @PostMapping("/events")
+    public ResponseEntity<RoomEvent> save(@RequestBody @Valid RoomEventDTO newEvent) {
+
+        Room room = roomService.findById(newEvent.getRoomId()).orElseThrow();
+        RoomEvent eventToSave = newEvent.toEntity();
+        eventToSave.setRoom(room);
+
+        return ResponseEntity.ok().body(eventService.save(eventToSave));
     }
 
 }
