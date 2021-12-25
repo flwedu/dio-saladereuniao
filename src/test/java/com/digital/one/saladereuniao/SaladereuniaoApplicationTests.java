b3674febf8e9fbc101fab8fc0dbd6a769e5be662
Eduardo <edutraquino@gmail.com>
2021-12-25 10:16:45 -0300

    smoke test to verify if the application is loaded

--- src/test/java/com/digital/one/saladereuniao/SaladereuniaoApplicationTests.java
+++ src/test/java/com/digital/one/saladereuniao/SaladereuniaoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.digital.one.saladereuniao;
 
+import com.digital.one.saladereuniao.controler.RoomController;
+import com.digital.one.saladereuniao.controler.RoomEventController;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SaladereuniaoApplicationTests {
 
+	@Autowired
+	private RoomController roomController;
+
+	@Autowired
+	private RoomEventController eventRoomController;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(roomController).isNotNull();
+		Assertions.assertThat(eventRoomController).isNotNull();
 	}
 
 }
