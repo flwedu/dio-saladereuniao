6c8e3ec6f9101a6bd2f54087a43154046d5b44f3
Eduardo <edutraquino@gmail.com>
2021-12-26 10:45:12 -0300

    removing getEventsByRoomId method from controller

--- src/main/java/com/digital/one/saladereuniao/controler/RoomEventController.java
+++ src/main/java/com/digital/one/saladereuniao/controler/RoomEventController.java
@@ -14,10 +14,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,18 +37,8 @@ public class RoomEventController {
     @GetMapping("/events")
     public ResponseEntity<Page<RoomEvent>> getAllEvents(@RequestParam(defaultValue = "0") Integer page) {
 
-        PageRequest page = PageRequest.of(pageNumber, 10);
-        return ResponseEntity.ok().body(eventService.findAll(page));
-    }
-
-    @GetMapping("/rooms/{roomId}/events/page/{pageNumber}")
-    public ResponseEntity<Page<RoomEvent>> getEventsByRoom(@PathVariable("roomId") Long roomId,
-            @PathVariable("pageNumber") int pageNumber) {
-
-        PageRequest page = PageRequest.of(pageNumber, 10);
-
-        return ResponseEntity.ok().body(eventService.findAllByRoomId(roomId, page));
-
+        PageRequest pageRequest = PageRequest.of(page, 10);
+        return ResponseEntity.ok().body(eventService.findAll(pageRequest));
     }
 
     @PostMapping("/events")
