f292302306cf4926ac3149e83921e27a8d2b545c
Eduardo <edutraquino@gmail.com>
2021-12-26 10:19:59 -0300

    add fetchtype.LAZY to relations

--- src/main/java/com/digital/one/saladereuniao/model/RoomEvent.java
+++ src/main/java/com/digital/one/saladereuniao/model/RoomEvent.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -31,7 +32,7 @@ public class RoomEvent {
     private String name;
     private String description;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     Room room;
 
     @Column(nullable = false)
