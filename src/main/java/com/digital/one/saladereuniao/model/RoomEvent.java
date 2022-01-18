4b0201a24124220c9e83909fb6cc7386aa3681de
Eduardo Aquino <edutraquino@gmail.com>
2022-01-18 19:07:46 -0300

    remove fetch = FetchType.LAZY

--- src/main/java/com/digital/one/saladereuniao/model/RoomEvent.java
+++ src/main/java/com/digital/one/saladereuniao/model/RoomEvent.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -32,7 +31,7 @@ public class RoomEvent {
     private String name;
     private String description;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne()
     Room room;
 
     @Column(nullable = false)
